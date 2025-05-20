34826704e1c0b7ddd3e569cef4c3719af320d0c8
Malak Elyan <malakelyan02@gmail.com>
2025-05-20 20:09:20 +0000

    Update file routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,14 @@
 from flask import Blueprint, jsonify, request
+from .db import get_db_connection
 
 todo_bp = Blueprint('todo', __name__)
 
-todos = []
-
-@todo_bp.route('/', methods=['GET'])
-def home():
-    return jsonify({'message': 'Welcome to the Flask Todo API!'})
-
-
 @todo_bp.route('/todos', methods=['GET'])
 def get_todos():
-    return jsonify(todos)
-
-@todo_bp.route('/todos', methods=['POST'])
-def add_todo():
-    data = request.get_json()
-    todos.append(data)
-    return jsonify({'message': 'Todo added!'}), 201
-
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT id, task FROM todos;')
+    todos = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify([{'id': t[0], 'task': t[1]} for t in todos])
