ba8e4b2c91ab75817b1072b58800f5e38443e8a8
Malak Elyan <malakelyan02@gmail.com>
2025-05-31 15:14:14 +0000

    Update 2 files
    - /app/templates/index.html
    - /app/routes.py

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,12 @@ todo_bp = Blueprint('todo', __name__)
 
 @todo_bp.route('/')
 def index():
-    return render_template('index.html')
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT id, task FROM todos')
+    todos = cur.fetchall()
+    conn.close()
+    return render_template('index.html', todos=todos)
 
 @todo_bp.route('/todos', methods=['GET'])
 def get_todos():
@@ -30,3 +35,23 @@ def add_todo():
     new_id = cur.lastrowid
     conn.close()
     return jsonify({'id': new_id, 'task': task}), 201
+
+@todo_bp.route('/add', methods=['POST'])
+def add_todo_html():
+    task = request.form.get('task')
+    if task:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('INSERT INTO todos (task) VALUES (?);', (task,))
+        conn.commit()
+        conn.close()
+    return redirect(url_for('todo.index'))
+
+@todo_bp.route('/delete/<int:todo_id>')
+def delete_todo(todo_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM todos WHERE id = ?', (todo_id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('todo.index'))
