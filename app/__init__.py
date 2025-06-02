e9205740e6dfbb7a003e8650a2d2f127a8d337f5
ملاك محمد حسن عليان Elyan <melyan1@smail.ucas.edu.ps>
2025-06-02 13:54:22 +0000

    Update file __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,10 @@
 from flask import Flask
 from .routes import todo_bp
-from .db import get_db_connection
-
+from .db import get_db_connection, init_db  # ✅ استيراد الدوال
 
 def create_app():
     app = Flask(__name__)
-
-    from .routes import todo_bp
     app.register_blueprint(todo_bp)
-
-    # ربط دالة الاتصال بالتطبيق
-    app.config['GET_DB_CONNECTION'] = get_db_connection
-
-    # إنشاء الجدول عند التشغيل
-    with app.app_context():
-        init_db()
-
+    app.config['GET_DB_CONNECTION'] = get_db_connection # ربط دالة اللاتصال بالتطبيق
+    init_db()  # ✅ تهيئة قاعدة البيانات
     return app
