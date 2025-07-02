ecb4d1e44bce22f482ab92c20fa71707885290f3
Malak Elyan <malakelyan02@gmail.com>
2025-07-02 07:56:39 +0000

    Update file db.py

--- app/db.py
+++ app/db.py
@@ -4,6 +4,12 @@ import os
 def get_db_connection():
     return psycopg2.connect(os.environ['DATABASE_URL'])
 
+    def get_db_connection():
+    database_url = os.environ.get('DATABASE_URL')
+    if not database_url:
+        raise Exception("DATABASE_URL environment variable not set")
+    return psycopg2.connect(database_url)
+
 def init_db():
     conn = get_db_connection()
     cur = conn.cursor()
